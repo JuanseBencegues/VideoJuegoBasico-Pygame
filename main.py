6078aef7e8c65ca36cff65fb7e3d0ec8fd45ebdf
JuanseBencegues <benceguesj@gmail.com>
2025-08-01 15:56:27 -0300

    Se añadio movimiento para el jugador

--- main.py
+++ main.py
@@ -7,13 +7,45 @@ jugador  = Personaje(50,50)
 pygame.init()
 
 ventana = pygame.display.set_mode((constantes.ANCHO_VENTANA, constantes.ALTO_VENTANA)) #Inicia ventana
+run = True
 
 #nombre de la ventana
 pygame.display.set_caption("Juego")
 
-run = True
+#definir variables de movimiento del jugador
+mover_arriba = False
+mover_abajo = False
+mover_derecha = False
+mover_izquierda = False
+
+#Controlar frame-rate
+reloj = pygame.time.Clock()
+
+
 
 while run:
+    #determinar fps
+    reloj.tick(constantes.FPS)
+
+
+    ventana.fill(constantes.COLOR_FONDO)
+
+    #Calcular movimiento jugador
+    delta_x = 0
+    delta_y = 0
+
+    if mover_derecha == True:
+        delta_x = constantes.VELOCIDAD
+    if mover_izquierda == True:
+        delta_x = -constantes.VELOCIDAD
+    if mover_arriba == True:
+        delta_y = -constantes.VELOCIDAD
+    if mover_abajo == True:
+        delta_y = constantes.VELOCIDAD
+
+    #mover al jugador
+    jugador.movimiento(delta_x, delta_y)
+
     jugador.dibujar(ventana)
     #recorre los posibles distintos eventos que se ejecuten 
     for event in pygame.event.get(): #event.get entrega la lista de todos los eventos que pueden ocurrir
@@ -21,6 +53,28 @@ while run:
             #event.type muestra que tipo de evento es 
             #pygame.QUIT accion de cerrar ventana (ej. click en la cruz)
             run = False
+        #Cuando se precione una tecla
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_a: #Se presionó 'a'
+                mover_izquierda = True
+            if event.key == pygame.K_d: #Se presionó 'd'
+                mover_derecha = True
+            if event.key == pygame.K_w: #Se presionó 'w'
+                mover_arriba = True
+            if event.key == pygame.K_s: #Se presionó 's'
+                mover_abajo = True
+        #Cuando se suelte una tecla
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a: 
+                mover_izquierda = False
+            if event.key == pygame.K_d: 
+                mover_derecha = False
+            if event.key == pygame.K_w: 
+                mover_arriba = False
+            if event.key == pygame.K_s: 
+                mover_abajo = False
+
+
     pygame.display.update() #refleja cambios
 
 pygame.quit()
--- personaje.py
+++ personaje.py
@@ -6,6 +6,12 @@ class Personaje():
         self.forma = pygame.Rect( 0, 0 , constantes.ANCHO_PERSONAJE, constantes.ALTO_PERSONAJE) #crea rectangulo donde (coordenada x, coord y, ancho, alto)
         self.forma.center = (x, y) #Cuando creo personaje, se inicia en las coordenadas dadas
     
+    def movimiento(self, delta_x, delta_y):
+            #moverse en horizontal
+            self.forma.x = self.forma.x + delta_x
+            #moverse en vertical
+            self.forma.y = self.forma.y + delta_y
+            
     def dibujar(self, interfaz):
         #dibujar rectangulo en la ventanda (interfaz) con el color y la forma 
         pygame.draw.rect(interfaz,constantes.COLOR_PERSONAJE,self.forma)
